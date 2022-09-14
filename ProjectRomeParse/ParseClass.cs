5385cb878346d43aa08e36373fe117b5492e956a
Глеб Бутяев <iiaiias1992@gmail.com>
2022-09-14 15:36:05 +0300

    Пока рано

---
+++ ProjectRomeParse/ParseClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pars_Rome
+{
+    public static class ParseClass
+    {
+
+        private static readonly Dictionary<char, int> romanDigits =
+        new Dictionary<char, int> {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+        public static int Decode(string s)
+        {
+            int result = 0;
+            for (int i = s.Length - 1; i > 0; i--)
+            { 
+            int curr = romanDigits.Get 
+
+
+            }
+            
+
+        }
+}
--- ProjectRomeParse/ParseRome.cs
+++ ProjectRomeParse/ParseRome.cs
@@ -1,4 +1,4 @@
-﻿internal class Program
+﻿internal class ParseRome
 {
     private static void Main(string[] args)
     {
